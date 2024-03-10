b337de9a02c3fa7d02d639094e51b040a48fbccd
mrkyaing <akog02@gmail.com>
2024-03-10 20:24:54 +0630

    add DailyAttendanceEntity to the models.

---
+++ EWADB3/CloudHRMS/Models/DataModels/DailyAttendanceEntity.cs
@@ -0,0 +1,19 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+
+namespace CloudHRMS.Models.DataModels
+{
+    [Table("DailyAttendance")]
+    public class DailyAttendanceEntity:BaseEntity
+    {
+        public DateTime AttenanceDate { get; set; }
+        public TimeSpan InTime { get; set; }
+        public TimeSpan OutTime { get; set; }
+        [ForeignKey("EmployeeId")]
+        public string EmployeeId { get; set; }
+        public virtual EmployeeEntity Employee { get; set; }
+        [ForeignKey("DepartmentId")]
+        public string DepartmentId { get; set; }
+        public virtual DepartmentEntity Department { get; set; }
+
+    }
+}
