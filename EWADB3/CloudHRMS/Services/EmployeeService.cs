6300aa58819e50e07c92837a53670e3af908e2a6
mrkyaing <akog02@gmail.com>
2024-03-10 20:14:06 +0630

    add cloudHRMS project to the repo.

---
+++ EWADB3/CloudHRMS/Services/EmployeeService.cs
@@ -0,0 +1,179 @@
+﻿using CloudHRMS.Models.DataModels;
+using CloudHRMS.Models.ViewModels;
+using CloudHRMS.ReportHelper;
+using CloudHRMS.Repostories;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.ReportingServices.ReportProcessing.ReportObjectModel;
+
+namespace CloudHRMS.Services
+{
+    public class EmployeeService : IEmployeeService
+    {
+        private readonly IEmployeeRepository _employeeRepository;
+        private readonly IPositionRepository _positionRepository;
+        private readonly IDepartmentRepository _departmentRepository;
+        private readonly UserManager<IdentityUser> _userManager;
+        public EmployeeService(UserManager<IdentityUser> userManager, IEmployeeRepository employeeRepository,IPositionRepository positionRepository,IDepartmentRepository departmentRepository)
+        {
+            _userManager = userManager;
+            this._employeeRepository = employeeRepository;
+            this._positionRepository = positionRepository;
+            this._departmentRepository = departmentRepository;
+        }
+        public async Task Create(EmployeeViewModel ui,string profileUrl)
+        {
+            try
+            {
+                var IsPositionCodeAlreadyExists = _employeeRepository.GetAll().Where(w => w.Code == ui.Code).Any();
+                if(IsPositionCodeAlreadyExists)
+                {
+                    throw new Exception("Code already exists in the system.");
+                }
+                try
+                {
+                    var user = new IdentityUser { UserName = ui.Email, Email = ui.Email };
+                    var result = await _userManager.CreateAsync(user, "CloudHRMS@prodev@123");
+                    if (result.Succeeded)
+                    {
+                        await _userManager.AddToRoleAsync(user, "EMPLOYEE");
+                    }
+
+                    //Data exchange from view model to data model
+                    var employee = new EmployeeEntity()
+                    {
+                        Id = ui.Id,//update the recrod with  the existing id 
+                        Code = ui.Code,
+                        Name = ui.Name,
+                        Email = ui.Email,
+                        Phone = ui.Phone,
+                        DOB = ui.DOB,
+                        DOE = ui.DOE,
+                        Address = ui.Address,
+                        BasicSalary = ui.BasicSalary,
+                        Gender = ui.Gender,
+                        ModifiedAt = DateTime.Now,
+                        ProfilePicture = profileUrl,
+                        DepartmentId = ui.DepartmentId,
+                        PositionId = ui.PositionId,
+                        UserId= user.Id
+                    };
+                    _employeeRepository.Create(employee);
+                }
+                catch (Exception ex)
+                {
+                    throw;
+                }
+                
+            }       
+            catch (Exception)
+            {
+                throw;
+            }
+        }
+        public void Delete(string id)
+        {
+            _employeeRepository.Delete(id);
+        }
+
+        public IList<EmployeeViewModel> GetAll()
+        {
+            try
+            {
+                var employees = (from e in _employeeRepository.GetAll()
+                                 join d in _departmentRepository.GetAll()
+                                 on e.DepartmentId equals d.Id
+                                 join p in _positionRepository.GetAll()
+                                 on e.PositionId equals p.Id
+                                 select new EmployeeViewModel
+                                 {
+                                     Id = e.Id,
+                                     Name = e.Name,
+                                     Email = e.Email,
+                                     DOB = e.DOB,
+                                     BasicSalary = e.BasicSalary,
+                                     Address = e.Address,
+                                     Gender = e.Gender,
+                                     Phone = e.Phone,
+                                     Code = e.Code,
+                                     DOE = e.DOE,
+                                     DepartmentInfo = d.Name,
+                                     PositionInfo = p.Name,
+                                     ProfilePicture = e.ProfilePicture ?? ""
+                                 }).ToList();
+                return employees;
+            }
+            catch (Exception)
+            {
+
+                throw;
+            }
+        }
+        public IList<EmployeeDetail> GetByFromCodeToCodeDepartentId(string fromCode, string toCode, string DeprtmentId)
+        {
+            return (from e in _employeeRepository.GetAll()
+                    join d in _departmentRepository.GetAll()
+                    on e.DepartmentId equals d.Id
+                    join p in _positionRepository.GetAll()
+                    on e.PositionId equals p.Id
+                    where (e.Code.CompareTo(fromCode)>=0&&e.Code.CompareTo(toCode)<=0) || e.DepartmentId==DeprtmentId
+                    select new EmployeeDetail
+                    {
+                        Name = e.Name,
+                        Email = e.Email,
+                        DOB = e.DOB,
+                        BasicSalary = e.BasicSalary,
+                        Address = e.Address,
+                        Gender = e.Gender,
+                        Phone = e.Phone,
+                        Code = e.Code,
+                        DOE = e.DOE,
+                        DepartmentInfo = d.Name,
+                        PositionInfo = p.Name,
+                        ProfilePicture = e.ProfilePicture ?? ""
+                    }).ToList();
+        }
+        public EmployeeViewModel GetById(string id)
+        {
+            var e = _employeeRepository.GetById(id);
+            var d = _departmentRepository.GetById(e.DepartmentId);
+            var p = _positionRepository.GetById(e.PositionId);
+           return new EmployeeViewModel()
+            {
+               Id = e.Id,
+               Name = e.Name,
+               Email = e.Email,
+               DOB = e.DOB,
+               BasicSalary = e.BasicSalary,
+               Address = e.Address,
+               Gender = e.Gender,
+               Phone = e.Phone,
+               Code = e.Code,
+               DOE = e.DOE,
+               DepartmentInfo = d.Name,
+               PositionInfo = p.Name,
+               ProfilePicture = e.ProfilePicture ?? ""
+           };
+        }
+        public void Update(EmployeeViewModel ui)
+        {
+            //Data exchange from view model to data model
+            var employee = new EmployeeEntity()
+            {
+                Id = ui.Id,//update the recrod with  the existing id 
+                Code = ui.Code,
+                Name = ui.Name,
+                Email = ui.Email,
+                Phone = ui.Phone,
+                DOB = ui.DOB,
+                DOE = ui.DOE,
+                Address = ui.Address,
+                BasicSalary = ui.BasicSalary,
+                Gender = ui.Gender,
+                ModifiedAt = DateTime.Now,
+                DepartmentId = ui.DepartmentId,
+                PositionId = ui.PositionId
+            };
+            _employeeRepository.Update(employee);
+        }
+    }
+}
---
+++ EWADB3/HelloWorldMVC/Controllers/HomeController.cs
@@ -0,0 +1,75 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace HelloWorldMVC.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly IWebHostEnvironment _webHostEnvironment;
+        public HomeController(IWebHostEnvironment webHostEnvironment)
+        {
+            _webHostEnvironment = webHostEnvironment;
+            
+        }
+        //http://localhost:3000/home/index
+        public IActionResult Index()
+        {
+            int hour = DateTime.Now.Hour;
+            string message = hour > 12 ? "Good Afternoon,Time is " + DateTime.Now.ToShortTimeString() 
+                                                                : "Good Morning,Time is " + DateTime.Now.ToShortTimeString();
+            ViewBag.TimeInfo = message;
+            return View();
+        }
+        //http://locahost:3000/home/SayHello
+        public string SayHello()
+        {
+            return "Hi, i am new developer with mvc";
+        }
+        //http://locahost:3000/home/TellMeTime
+        public string TellMeTime()
+        {
+            return DateTime.Now.ToShortTimeString();
+        }
+
+        public ViewResult List()
+        {
+            ViewBag.Friends = new string[] { "Mg Mg ","Su Su","Aye Aye"};
+            return View();
+        }
+        //http://locahost:3000/home/Add?n1=10&n2=10
+        public string Add(int n1,int n2)
+        {
+            return $"Calculated Result:{(n1 + n2).ToString()}";
+        }
+
+        public FileResult Download(){
+            string fileName="KAPYINFOS.png";
+            string filePath=Path.Combine(_webHostEnvironment.WebRootPath,"DownloadFiles//"+fileName);
+            byte[] byteInfFile=System.IO.File.ReadAllBytes(filePath);
+            return File(byteInfFile,"text/png",fileName);
+        }
+        //localhost:5082/home/fullname?firstname=Mg&lastname=Mg
+        public ViewResult FullName(string firstName,string lastName){
+            string FullName="Hello,"+firstName+" " +lastName;
+            ViewBag.FullNameInfo=FullName;
+            return View();
+        }
+        //home/iam
+        [ActionName("iam")]
+        public IActionResult SayYourName(){
+            return View("me");
+        }
+         //home/Hi
+        public int Hi()=>30;
+       
+        [NonAction]
+         public int HiHi()=>60;
+         
+        //http://locahost:3000/home/TaxCalculate
+        [HttpPost]
+         public string TaxCalculate(decimal TotalPrice,int Pertance){
+            decimal taxReuslt=TotalPrice/100*Pertance;
+            return $"Tax Amount {taxReuslt}";
+         }
+
+    }
+}
